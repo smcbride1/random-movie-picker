876bdfe2a8291e3c8e0f3612b987651e861d4f81
Stephen McBride <stevemcbride3@gmail.com>
2020-05-05 15:59:06 -0400

    Added attr_accessor and initialze method to Movie class

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,9 @@
 class Movie
+
+    attr_accessor :title, :year, :rating, :genre, :runtime, :description, :service_urls
+
+    def initialize(attributes)
+        attributes.each { |key, value| self.send(("#{key}="), value) }
+    end
+
 end
