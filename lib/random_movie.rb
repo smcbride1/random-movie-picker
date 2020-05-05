5b95ee1f88a6ffd51251792d3695032de91460c5
Stephen McBride <stevemcbride3@gmail.com>
2020-05-05 17:27:46 -0400

    Changed and renamed self.cache_genres and self.cache_years to self.genres and self.years in RandomMovie class; they now act as getters that return the class var if it is not empty, and fetches data and sets the var if it is.

--- lib/random_movie.rb
+++ lib/random_movie.rb
@@ -9,20 +9,40 @@ class RandomMovie
     @@genres = []
     @@years = []
 
-    def self.cache_genres
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/"))
-        @@genres = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |genre|
-            {:name => white_space_fix(genre.text),
-             :url => genre.attribute("href").text}
+    def self.random_by_genre(genre)
+        doc = Nokogiri::HTML(open(@@genres[genre]))
+
         end
     end
 
-    def self.cache_years
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2020"))
-        @@years = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |year|
-            {:name => white_space_fix(year.text),
-             :url => year.attribute("href").text}
+    def self.random_by_year(year)
+    end
+
+    def self.genres
+
+        if @@genres = []
+            doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/"))
+            @@genres = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |genre|
+                {white_space_fix(genre.text) => genre.attribute("href").text}
+            end
+        else
+            @@genres
         end
+        
+    end
+
+    def self.years
+
+        if @@years = []
+            doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2020"))
+            @@years = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |year|
+                {:name => white_space_fix(year.text),
+                 :url => year.attribute("href").text}
+            end
+        else
+            @@years
+        end
+
     end
 
     def self.white_space_fix(text)
