cbd164f2f24a8034521f7b4ceaae0faf86372f96
Stephen McBride <stevemcbride3@gmail.com>
2020-05-05 17:47:32 -0400

    Added an @@all class variable and getter to the Movie class and added the url and fully_fetched attributes

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,9 +1,17 @@
 class Movie
 
-    attr_accessor :title, :year, :rating, :genre, :runtime, :description, :service_urls
+    attr_accessor :title, :year, :rating, :genre, :runtime, :description, :url, :service_urls, :fully_fetched
+
+    @@all = []
 
     def initialize(attributes)
+        @fully_fetched = false
         attributes.each { |key, value| self.send(("#{key}="), value) }
+        self.class.push(self)
+    end
+
+    def self.all
+        @@all
     end
 
     def formatted_info
