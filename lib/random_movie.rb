cd7623c2da60e6eb5980e86ddf5c17097aff3107
Stephen McBride <stevemcbride3@gmail.com>
2020-05-05 16:48:45 -0400

    Added self.cache_years method to RandomMovie class

--- lib/random_movie.rb
+++ lib/random_movie.rb
@@ -17,4 +17,12 @@ class RandomMovie
         end
     end
 
+    def self.cache_years
+        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/"))
+        @@genres = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |genre|
+            {:name => genre.text.split("\n")[1].strip,
+             :url => genre.attribute("href").text}
+        end
+    end
+
 end
