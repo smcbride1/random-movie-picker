a54af7b30cb9c9e655d815b6d6d494a9d191971d
Stephen McBride <stevemcbride3@gmail.com>
2020-05-05 18:15:38 -0400

    Changed @@genres and @@years to be a hash, not an array of hashes; also added the base url to the url segment when the hash is created in the RandomMovie class

--- lib/random_movie.rb
+++ lib/random_movie.rb
@@ -1,44 +1,40 @@
 require "open-uri"
 require "nokogiri"
 require "pry"
+require_relative "./movie.rb"
+require_relative "../concerns/finder.rb"
 
 class RandomMovie
 
+    extend Finder
+
     BASE_URL = "https://www.rottentomatoes.com"
 
-    @@genres = []
-    @@years = []
-
-    def self.random_by_genre(genre)
-        doc = Nokogiri::HTML(open(@@genres[genre]))
-
-        end
-    end
-
-    def self.random_by_year(year)
-    end
+    @@genres = {}
+    @@years = {}
 
     def self.genres
 
-        if @@genres = []
+        if @@genres = {}
             doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/"))
-            @@genres = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |genre|
-                {white_space_fix(genre.text) => genre.attribute("href").text}
+            doc.css(".btn-group.btn-primary-border-dropdown li a").each do |genre|
+                @@genres[white_space_fix(genre.text)] = BASE_URL + genre.attribute("href").text
             end
+            @@genres
         else
             @@genres
         end
-        
+
     end
 
     def self.years
 
-        if @@years = []
+        if @@years = {}
             doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2020"))
             @@years = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |year|
-                {:name => white_space_fix(year.text),
-                 :url => year.attribute("href").text}
+                @@years[white_space_fix(year.text)] = BASE_URL + year.attribute("href").text
             end
+            @@years
         else
             @@years
         end
@@ -48,5 +44,7 @@ class RandomMovie
     def self.white_space_fix(text)
         text.split("\n")[1].strip
     end
+
+    #binding.pry
 
 end
