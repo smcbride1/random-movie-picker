fd55295d803dce9d98add88275f57db451c7025f
Stephen McBride <stevemcbride3@gmail.com>
2020-05-05 17:17:10 -0400

    Moved white space fix into its own method in RandomMovie class

--- lib/random_movie.rb
+++ lib/random_movie.rb
@@ -12,17 +12,21 @@ class RandomMovie
     def self.cache_genres
         doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/"))
         @@genres = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |genre|
-            {:name => genre.text.split("\n")[1].strip,
+            {:name => white_space_fix(genre.text),
              :url => genre.attribute("href").text}
         end
     end
 
     def self.cache_years
-        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2018"))
+        doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2020"))
         @@years = doc.css(".btn-group.btn-primary-border-dropdown li a").map do |year|
-            {:name => year.text.split("\n")[1].strip,
+            {:name => white_space_fix(year.text),
              :url => year.attribute("href").text}
         end
+    end
+
+    def self.white_space_fix(text)
+        text.split("\n")[1].strip
     end
 
 end
