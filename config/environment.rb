809eca4d10c7f1ef2407f592d57d1866cf23b5c4
Stephen McBride <stevemcbride3@gmail.com>
2020-05-07 04:53:13 -0400

    All CLI options functional

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+require 'launchy'
+require_relative '../lib/random_movie_picker/categories'
+require_relative '../lib/random_movie_picker/cli'
+require_relative '../lib/random_movie_picker/movie'
+require_relative '../lib/random_movie_picker/version'
---
+++ lib/random_movie_picker/version.rb
@@ -0,0 +1,3 @@
+module RandomMoviePicker
+  VERSION = "0.1.0"
+end
